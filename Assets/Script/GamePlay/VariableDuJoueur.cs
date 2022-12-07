70051076ad1b1e6dd996ad900bb380182094484d
Owenbernard <80883159+Owenbernard@users.noreply.github.com>
2022-12-07 10:52:51 +0100

    variableJoueur- 1ere personne Camera

---
+++ Assets/Script/GamePlay/VariableDuJoueur.cs
@@ -0,0 +1,20 @@
+//Owen bernard
+//variable de base du joueur
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableDuJoueur : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
