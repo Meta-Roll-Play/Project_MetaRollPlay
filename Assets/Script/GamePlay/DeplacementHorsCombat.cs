167595e8b851620ec1528441aa3288910e9d27d3
Owenbernard <80883159+Owenbernard@users.noreply.github.com>
2022-12-07 11:52:30 +0100

    Deplacement du joueur

--- Assets/Script/GamePlay/DeplacementHorsCombat.cs
+++ Assets/Script/GamePlay/DeplacementHorsCombat.cs
@@ -3,20 +3,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class DeplacementHorsCombat : MonoBehaviour
 {
 
-    [Header("Rotation du personnage")]
+    public CharacterController controller;
+    [Header("Attribut")]
+    public int Life;
+
+    [Header("Movement")]
+    private float flatSpeed = 5f;
+    public float speed = 0f;
+
+    [Header("Camera")]
     public Transform orientation;
     public float rotationSpeed;
+    public Transform camer;
 
+
+    Vector3 velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*lock du curseur
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        */
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //bloquage du curseur
+        //visibilité de la souris
         if (Input.GetMouseButton(1))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -28,36 +50,30 @@ public class DeplacementHorsCombat : MonoBehaviour
             Cursor.visible = false;
         }
 
-        /*
-        if (GetComponent<VariableDuJoueur>().velocity.y < 0)
+
+        if (velocity.y < 0)
         {
-            GetComponent<VariableDuJoueur>().velocity.y = -2f;
+            velocity.y = -2f;
         }
 
-        //si le joueur à acces et qu'il n'est pas en combat
-        if (GetComponent<VariableDuJoueur>().acces)
+        //input ZQSD
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //rotate
+        Vector3 viewDirection = transform.position - new Vector3(camer.position.x, transform.position.y, camer.position.z);
+        orientation.forward = viewDirection.normalized;
+
+
+        //set up en vector3
+        Vector3 move = orientation.forward * z + orientation.right * x;
+
+        //si pas immobile
+        if (move != Vector3.zero)
         {
-            if (!GetComponent<VariableDuJoueur>().modeCombat)
-            {
-
-                //input ZQSD
-                float x = Input.GetAxis("Horizontal");
-                float z = Input.GetAxis("Vertical");
-
-                //orientation en fonction de la souris
-                Vector3 viewDirection = transform.position - new Vector3(MouseCursor.position.x, transform.position.y, MouseCursor.position.z);
-                orientation.forward = viewDirection.normalized;
-
-                Vector3 move = orientation.forward * z + orientation.right * x;
-
-                //mouvement du personnage
-                if (move != Vector3.zero)
-                {
-                    //deplacement
-                    GetComponent<VariableDuJoueur>().controllePlayer.Move(move * GetComponent<VariableDuJoueur>().speed * Time.deltaTime);
-                    transform.forward = Vector3.Lerp(transform.forward, move.normalized, Time.deltaTime * rotationSpeed);
-                }
-            }
-        }*/
+            //deplacement
+            controller.Move(move * speed * Time.deltaTime);
+            transform.forward = Vector3.Lerp(transform.forward, move.normalized, Time.deltaTime * rotationSpeed);
+        }
     }
 }
--- Assets/Script/GamePlay/VariableDuJoueur.cs
+++ Assets/Script/GamePlay/VariableDuJoueur.cs
@@ -21,4 +21,11 @@ public class VariableDuJoueur : MonoBehaviour
     public bool acces;
     public bool modeCombat;
 
+
+    [Header("Rotation du personnage")]
+    public Transform orientation;
+    public float rotationSpeed;
+    public float mouseSensitivityX = 100f;
+    public float mouseSensitivityY = 100f;
+
 }
